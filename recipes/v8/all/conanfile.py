04422d615650e4c3c56a6cb23d10e15170fe4d93
Paul Harris <harris.pc@gmail.com>
2022-07-20 23:35:49 +0800

    v8 - adjust to use a newer Windows SDK
    It isn't what v8 was tested with, but it is what I'm using.

--- recipes/v8/all/conanfile.py
+++ recipes/v8/all/conanfile.py
@@ -194,6 +194,16 @@ class V8Conan(ConanFile):
             "//build/config/win:default_crt",
             "//build/config/conan/msvc:conan_crt"
         )
+
+        # Assume the most recent Windows SDK is installed,
+        # otherwise v8 will assume the old SDK from msvc2019 era
+        # v8 wants to only use SDKs that it has tested, but I want to use a newer SDK with 2022
+        win_setup_toolchain_file = os.path.join(v8_source_root, "build", "toolchain", "win", "setup_toolchain.py")
+        tools.replace_in_file(win_setup_toolchain_file,
+            "10.0.20348.0",
+            "10.0.22621.0"
+        )
+
         # fix bug in BUILD.gn, was defining a header-target as a lib-target
         build_gn_file = os.path.join(v8_source_root, "BUILD.gn")
         tools.replace_in_file(build_gn_file,
